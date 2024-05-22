04a41aa9661de162f9b64e8823422b7c7d890750
Juca Crispim <jucacrispim@gmail.com>
2024-05-22 20:46:42 -0300

    Change ci confs

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf dist/
+
+python -b build
+
+cd dist
+
+fname=`ls | grep tar`
+
+curl -F file=@$fname -F prefix=pypi/asyncblink $PYPI_UPLOAD_URL -H "Authorization: Key $TUPI_AUTH_KEY"
